7a9fbf1ff1fd39758d26746b22370966b0df8a6b
Eduard Gataullin <edikgat@gmail.com>
2016-10-16 18:18:55 +0300

    emails validations

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,7 +2,6 @@ class AdminUser < ActiveRecord::Base
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :email, presence: true
   validates :password, :password_confirmation, presence: true, on: :create
   validates :password, confirmation: true
 end
--- app/models/support_agent.rb
+++ app/models/support_agent.rb
@@ -3,8 +3,7 @@ class SupportAgent < ActiveRecord::Base
   validates :email, presence: true
   validates :password, :password_confirmation, :first_name, :last_name, presence: true, on: :create
   validates :password, confirmation: true
-  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: { case_sensitive: false }, email: true, allow_blank: true
 
   devise :database_authenticatable
-
 end
