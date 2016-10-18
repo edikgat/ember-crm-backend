a137c9bce98604b13b1c5823bb9628bd46ff03cc
Eduard Gataullin <edikgat@gmail.com>
2016-10-18 07:40:44 +0300

    fixes

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,5 @@
 require "redis"
 
-
 current_redis_config = YMLConfigReader.fetch(Rails.root.join('config/redis.yml'), Rails.env)
 
 $redis = Redis::Namespace.new(Rails.env, redis: Redis.new(current_redis_config))
--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,3 +1,3 @@
 GrapeSwaggerRails.options.url = "/api/v1/swagger_doc"
-GrapeSwaggerRails.options.app_name = "CrossoverDemoDoc"
+GrapeSwaggerRails.options.app_name = "ApiDoc"
 GrapeSwaggerRails.options.app_url = "http://localhost:3000"
